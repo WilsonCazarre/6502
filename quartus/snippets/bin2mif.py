cd0d6e8ff1f0ca13ae47adc09c7658ed10b54d08
WilsonCazarre <wcs0486@gmail.com>
2024-09-16 23:40:11 -0300

    pc4

--- quartus/snippets/bin2mif.py
+++ quartus/snippets/bin2mif.py
@@ -1,20 +1,26 @@
+import subprocess
+import sys
+
 WIDTH = 8
+
+subprocess.run(["vasm", sys.argv[1], "-Fbin", "-dotdir"])
 
 with open("a.out", "rb") as in_file:
     data = bytearray(in_file.read())
 
 
-with open("a.mif", "w") as out_file:
-    code = [f"{address:04x}:{value:02x};\n" for address, value in enumerate(data)]
-    out_file.writelines(
-        [
-            f"DEPTH={len(code)};\n",
-            f"WIDTH={WIDTH};\n",
-            "ADDRESS_RADIX = HEX;\n",
-            "DATA_RADIX = HEX;\n",
-            "CONTENT\n",
-            "BEGIN\n",
-        ]
-    )
+with open("../simulation/rom.hex", "w") as out_file:
+    # code = [f"{address:04x}:{value:02x};\n" for address, value in enumerate(data)]
+    code = [f"{value:02x}\n" for address, value in enumerate(data)]
+    # out_file.writelines(
+    #     [
+    #         f"DEPTH={len(code)};\n",
+    #         f"WIDTH={WIDTH};\n",
+    #         "ADDRESS_RADIX = HEX;\n",
+    #         "DATA_RADIX = HEX;\n",
+    #         "CONTENT\n",
+    #         "BEGIN\n",
+    #     ]
+    # )
+    # out_file.writelines([code])
     out_file.writelines(code)
-    out_file.writelines(["END"])
--- quartus/snippets/fibo.s
+++ quartus/snippets/fibo.s
@@ -1,39 +1,57 @@
-DDRA=$0803
-DDRB=$0802
-PORTA=$0801
-PORTB=$0800
+; Registradores do adaptador de interface
+DDRA  = $0803
+DDRB  = $0802
+PORTA = $0801
+PORTB = $0800
 
-N1=$00
-N2=$01
-R=$02
+; Endereços para variáveis na RAM
+N1 = $00
+N2 = $01
+R  = $02
 
+  ; Usamos a diretriz .org para indicar para o assembler onde 
+  ; nosso programa começa no espaço de memória
   .org $8000
   
-  lda #$00 ; 0 - Input / 1 - Output
+  ; Define todos os bits de porta B como Entrada
+  lda #$00 ; 0 - Entrada / 1 - Saída
   sta DDRB
 
+  ; Define todos os bits de porta A como saída
   lda #$ff
   sta DDRA
 
+  
 restart:
-  lda #0
+  ; Inicializa N1 = 0, N2 = 1, R = 1
+  lda #$00
   sta N1
   lda #$01
   sta N2
   sta R
 loop:
+  ; Esse loop continua executando enquando (R - PORTB) > 0
+  ; Do contrário ele pula de volta para restart, para recalcular a sequência
   lda R
   cmp PORTB
-  bcs restart
-  sta PORTA
+  bpl restart
+  ; Exibe o valor R na PORTA, efetivamente mostrando a saída para o usuário
+  sta PORTA 
+
+  ; R = N1 + N2
   lda N1
   clc
   adc N2
   sta R
+
+  ; N1 = N2
   lda N2
   sta N1
+
+  ; N2 = R
   lda R
   sta N2
+
   jmp loop
 
 
